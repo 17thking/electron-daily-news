ff0234e1773e430457df9c437a96ffa87bc235ac
!_King <60766634+17thking@users.noreply.github.com>
2025-07-30 01:39:02 -0700

    Update main.js
    Fixed double bracket

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
 function createWindow() {
-  const win = new BrowserWindow{{
+  const win = new BrowserWindow({
     width: 1000,
       height: 700,
       webPreferences: {
